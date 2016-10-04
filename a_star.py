5770bc7319c9eb781bcd12b66e3ce0687e9b38fa
StianHanssen <Hanssen.Stian@gmail.com>
2016-10-04 13:07:01 +0200

    Fixed something something

--- a_star.py
+++ a_star.py
@@ -5,9 +5,9 @@ from heapq import heappush, heappop
 def A_star(board):
         opened = []
         closed = set()
-        heappush(opened, board.START)
+        heappush(opened, (board.START.get_f(), board.START))
         while opened:
-            cell = heappop(opened)
+            _, cell = heappop(opened)
             closed.add(cell)
             if cell is board.END:
                 return board.get_path()
@@ -18,9 +18,9 @@ def A_star(board):
                             board.update_cell(neighbour, cell)
                     else:
                         board.update_neightbour(neighbour, cell)
-                        heappush(opened, neighbour)
+                        heappush(opened, (neighbour.get_f(), neighbour))
 
 if __name__ == '__main__':
-    board = Board("board-1-2.txt")
+    board = Board("board-1-1.txt")
     print(board)
     print(A_star(board))
