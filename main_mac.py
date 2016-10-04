01caf2beb6338d7a84ab18806cdaf90f2efa833c
Stian Hanssen <Hanssen.Stian@gmail.com>
2016-10-05 00:41:38 +0200

    Customized mains to test time more accurately and work with new changes

--- main_mac.py
+++ main_mac.py
@@ -3,11 +3,32 @@ from cell import Cell
 from bfs import bfs
 from a_star import A_star
 
-if __name__ == '__main__':
-    board = Board("board-2-3.txt", True, True)
-    print(board)
-    print("\nA* time:", A_star(board), "s\n")
 
-    board = Board("board-2-4.txt", True, True)
+def averege_time(algorithm, board, cycles):
+    time_sum = 0
+    for _ in range(cycles):
+        new_board = board.clone()
+        time_sum += algorithm(new_board)[0]
+    return time_sum/cycles
+
+if __name__ == '__main__':
+    board = Board("board-2-3.txt", mac=True, emoji=True)
     print(board)
-    print("\nBFS time:", bfs(board), "s\n")
+    path = a_star(board)[1]
+    time = averege_time(a_star, board, 500)
+    print(path)
+    print("\nA* time:", time, "s\n")
+
+    board = Board("board-2-3.txt", inf, 0, True, True)
+    print(board)
+    path = bfs(board)[1]
+    time = averege_time(bfs, board, 500)
+    print(path)
+    print("\nBFS time:", time, "s\n")
+
+    board = Board("board-2-3.txt", inf, 0, True, True)
+    print(board)
+    time = averege_time(dijkstra, board, 500)
+    path = dijkstra(board)[1]
+    print(path)
+    print("\nDijkstra time:", time, "s\n")
--- main_win.py
+++ main_win.py
@@ -1,13 +1,37 @@
 from board import Board
 from cell import Cell
 from bfs import bfs
-from a_star import A_star
+from a_star import a_star
+from dijkstra import dijkstra
+from math import inf
+
+def averege_time(algorithm, board, cycles):
+    time_sum = 0
+    for _ in range(cycles):
+        new_board = board.clone()
+        time_sum += algorithm(new_board)[0]
+    return time_sum/cycles
 
 if __name__ == '__main__':
-    board = Board("board-1-2.txt")
-    print(board)
-    print("\nA* time:", A_star(board), "s\n")
+    board_name = "board-1-4.txt"
 
-    board = Board("board-2-4.txt")
+    board = Board(board_name)
     print(board)
-    print("\nBFS time:", bfs(board), "s\n")
+    path = a_star(board)[1]
+    time = averege_time(a_star, board, 500)
+    print(path)
+    print("\nA* time:", time, "s\n")
+
+    board = Board(board_name, default_g=inf, default_h=0)
+    print(board)
+    path = bfs(board)[1]
+    time = averege_time(bfs, board, 500)
+    print(path)
+    print("\nBFS time:", time, "s\n")
+
+    board = Board(board_name, default_g=inf, default_h=0)
+    print(board)
+    time = averege_time(dijkstra, board, 500)
+    path = dijkstra(board)[1]
+    print(path)
+    print("\nDijkstra time:", time, "s\n")
