c9e6d9d3ed470f059e8b99706982b9677c464004
Vebjørn Isaksen <vebjorn.isaksen@gmail.com>
2016-10-04 20:37:31 +0200

    Made bfs work

--- bfs.py
+++ bfs.py
@@ -1,31 +1,27 @@
 from time import time
 from board import Board
+from heapq import heappush, heappop
 from math import inf
 
 
 def bfs(board):
+    start_time = time()
     for cell in board.get_cells():
         cell.set_g(inf)
     board.START.set_g(0)
-    start_time = time()
 
-    visited = set([board.START])
-    queue = [board.START]
+    queue = []
     end = board.END
-
+    heappush(queue, board.START)
     while queue:
-        node = queue.pop(0)
-        if node == end:
-            board.print_path()
-            return time() - start_time
-        for neighbour in board.get_neighbours(node):
-            if neighbour not in visited and (neighbour.get_g() > node.get_g() + neighbour.WEIGHT):
-                print(queue)
-                board.update_neightbour(neighbour, node)
-                queue.append(neighbour)
-                visited.add(neighbour)
-
+        cell = heappop(queue)
+        for neighbour in board.get_neighbours(cell):
+            if neighbour.get_g() > cell.get_g() + neighbour.WEIGHT:
+                board.update_neighbour(neighbour, cell)
+                heappush(queue, neighbour)
+    board.print_path()
+    return time() - start_time
 if __name__ == '__main__':
-    board = Board("board-1-3.txt", True, True)
+    board = Board("board-2-4.txt", True, True)
     print(board)
     print("BFS time: " + str(bfs(board)))
