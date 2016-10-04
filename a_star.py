66dc1febebe41f84fb7f2ef7394e47607c788709
Stian Hanssen <Hanssen.Stian@gmail.com>
2016-10-05 00:43:48 +0200

    Made more readable and changed according to print string function

--- a_star.py
+++ a_star.py
@@ -3,22 +3,20 @@ from cell import Cell
 from heapq import heappush, heappop
 from time import time
 
-def A_star(board):
+
+def a_star(board):
         start_time = time()
-        opened = []
+        opened = [board.START]
         closed = set()
-        heappush(opened, (board.START.get_f(), board.START))
         while opened:
-            _, cell = heappop(opened)
-            closed.add(cell)
+            cell = heappop(opened)
             if cell is board.END:
-                board.print_path()
-                return time() - start_time
+                return time() - start_time, board.get_path_str()
+            closed.add(cell)
             for neighbour in board.get_neighbours(cell):
                 if neighbour.WEIGHT is not None and neighbour not in closed:
-                    if (neighbour.get_f(), neighbour) in opened:
-                        if neighbour.get_g() > cell.get_g() + neighbour.WEIGHT:
-                            board.update_neighbour(neighbour, cell)
-                    else:
+                    if neighbour not in opened:
                         board.update_neighbour(neighbour, cell)
-                        heappush(opened, (neighbour.get_f(), neighbour))
+                        heappush(opened, neighbour)
+                    elif neighbour.get_g() > cell.get_g() + neighbour.WEIGHT:
+                        board.update_neighbour(neighbour, cell)
