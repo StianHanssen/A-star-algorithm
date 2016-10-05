5bb6566bba0f22c38eefe849a9a22ac9f68944c6
StianHanssen <Hanssen.Stian@gmail.com>
2016-10-05 15:39:29 +0200

    Added documentation

--- a_star.py
+++ a_star.py
@@ -5,7 +5,7 @@ from time import time
 
 
 def a_star(board):
-    """Calculates shortest path on board using A* alogrithm.
+    """Calculates shortest path on board using the A* alogrithm.
     Args:
         board (Board): Board being the graph the alorithm will use
 
@@ -16,7 +16,7 @@ def a_star(board):
     board.START.set_g(0)  # As all cells have g = inf, the first cell is set to 0
     opened = [board.START]  # Initialize priority queue with start cell
     closed = set()  # A set to keep track of notes that have been checked
-    while opened:
+    while opened:  # While there are still cells to check out
         cell = heappop(opened)
         if cell is board.END:  # Once cell being check is end cell, we are done
             return time() - start_time, board.get_path_str(opened, closed)
--- dijkstra.py
+++ dijkstra.py
@@ -6,17 +6,22 @@ from math import inf
 
 
 def dijkstra(board):
-    start_time = time()
-    board.START.set_g(0)
-    closed = set()
-    queue = []
-    end = board.END
-    heappush(queue, board.START)
-    while queue:
-        cell = heappop(queue)
+    """Calculates shortest path on board using the Dijkstra alogrithm.
+    Args:
+        board (Board): Board being the graph the alorithm will use
+
+    Returns:
+        Tuple of form (time, string representation of board with path)
+    """
+    start_time = time()  # Sets start checkpoint for time
+    board.START.set_g(0)  # As all cells have g = inf, the first cell is set to 0
+    closed = set()  # A set to keep track of notes that have been checked
+    opened = [board.START]  # Initialize priority queue with start cell
+    while opened:  # While there are still cells to check out
+        cell = heappop(opened)
         closed.add(cell)
         for neighbour in board.get_neighbours(cell):
-            if neighbour.get_g() > cell.get_g() + neighbour.WEIGHT:
-                board.update_neighbour(neighbour, cell)
-                heappush(queue, neighbour)
-    return time() - start_time, board.get_path_str(queue, closed)
+            if neighbour.get_g() > cell.get_g() + neighbour.WEIGHT:  # Better path found
+                board.update_neighbour(neighbour, cell)  # Relax and set parent
+                heappush(opened, neighbour)  # Add unchecked cell to be checked out later
+    return time() - start_time, board.get_path_str(opened, closed)  # All nodes has been visited
